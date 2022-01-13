6bba0ebbf25e95a0896b54aed0ca4c793238a9d3
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-01-13 13:14:10 -0500

    Add the espeak engine into the tts module, hopefully.

--- tts.lisp
+++ tts.lisp
@@ -1,47 +1,3 @@
-;;; tts.lisp -- Common Lisp interface  to Emacspeak speech servers
-;;; $Id: tts.lisp 7078 2011-06-29 22:07:46Z tv.raman.tv $
-;;; $Author: tv.raman.tv $
-;;; Description:   Interface Common Lisp (SBCL)  to Emacspeak TTS servers
-;;; Keywords: stumpwm, Emacspeak, Audio Desktop
-;;{{{   LCD Archive entry:
-
-;;; LCD Archive Entry:
-;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
-;;; A speech interface to Emacs |
-;;; $Date: 2011-06-29 15:07:46 -0700 (Wed, 29 Jun 2011) $ |
-;;;  $Revision: 7078 $ |
-;;; Location undetermined
-;;;
-
-;;}}} 
-;;{{{   Copyright:
-
-;;; Copyright (C)  2011, T. V. Raman<raman@cs.cornell.edu>
-;;; All Rights Reserved.
-;;;
-;;; This file is not part of GNU Emacs, but the same permissions apply.
-;;;
-;;; GNU Emacs is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;;
-;;; GNU Emacs is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Emacs; see the file COPYING.  If not, write to
-;;; the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,MA 02110-1301, USA.
-
-;;}}} 
-;;{{{  Introduction:
-
-;;; Commentary:
-;;; Interface Common  Lisp  to Emacspeak TTS servers
-
-;;}}} 
 (in-package :stumpwm)
 ;;{{{  Settings
 
@@ -54,6 +10,9 @@
 (defvar *tts-dtk*
   (concatenate 'string   *emacspeak* "servers/dtk-exp")
   "DTK tcl server")
+(defvar *tts-espeak*
+  (concatenate 'string *emacspeak* "/servers/espeak")
+  "The Espeak location.")
 
 (defvar *tts-outloud*
   (concatenate 'string   *emacspeak* "servers/outloud")
